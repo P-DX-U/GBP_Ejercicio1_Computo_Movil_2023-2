15c11f06a58233bfaa5221970714245c022139af
P-GB-U <81974601+P-DX-U@users.noreply.github.com>
2023-04-05 21:25:34 -0600

    Initial Commit.

---
+++ app/src/main/java/com/third/gbp_ejercicio1_computo_movil_2023_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.third.gbp_ejercicio1_computo_movil_2023_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
