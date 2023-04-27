a4a807a9ea9679a45e92df50e3a0832aebe834ef
P-GB-U <81974601+P-DX-U@users.noreply.github.com>
2023-04-26 22:50:33 -0600

    Implementación de Spinner con las carreras de la FI UNAM.

--- app/src/main/java/com/third/gbp_ejercicio1_computo_movil_2023_2/MainActivity.kt
+++ app/src/main/java/com/third/gbp_ejercicio1_computo_movil_2023_2/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.third.gbp_ejercicio1_computo_movil_2023_2
 
+import android.R
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.third.gbp_ejercicio1_computo_movil_2023_2.databinding.ActivityMainBinding
+import com.third.gbp_ejercicio1_computo_movil_2023_2.R as localR
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val arrayCarreras: Array<String> = resources.getStringArray(localR.array.carreras)
+        val aaCarrerasFi = ArrayAdapter<String>(this,
+            R.layout.simple_spinner_dropdown_item)
+        binding.spCarrerasFi.adapter = aaCarrerasFi
+        aaCarrerasFi.addAll(arrayCarreras.toList())
     }
 
     fun dateClick(view: View){
